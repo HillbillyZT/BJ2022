06b5f067efc43764532705b49ff6166ebfc1144e
TheMysticalBard <parker.smyrl@gmail.com>
2022-08-24 22:24:40 -0500

    Chris i did a little input managering and fixed ur high dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,73 @@
 use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+#[derive(Component)]
+pub struct Player;
+
+#[derive(Component)]
+pub struct Entity {
+
+}
+
+#[derive(Actionlike, Clone)]
+pub enum Action {
+    Right,
+    Left,
+    Up,
+    Down,
+    Shoot,
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_plugin(InputManagerPlugin::<Action>::default())
         .add_startup_system(setup)
+        .add_system(handle_input)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(Camera2dBundle::default());
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("icon.png"),
-        ..Default::default()
-    });
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: asset_server.load("icon.png"),
+            ..Default::default()
+        })
+        .insert(Player)
+        .insert_bundle(InputManagerBundle::<Action> {
+            action_state: ActionState::default(),
+            input_map: InputMap::new([
+                (KeyCode::D, Action::Right),
+                (KeyCode::A, Action::Left),
+                (KeyCode::W, Action::Up),
+                (KeyCode::S, Action::Down),
+                (KeyCode::Space, Action::Shoot)
+            ]),
+        });
+}
+
+pub fn handle_input(mut query: Query<(&ActionState<Action>, &mut Transform), With<Player>>) {
+    let (action, mut transform) = query.single_mut();
+    let mut d_pos = Vec2::new(0.0, 0.0);
+    if action.just_pressed(Action::Shoot) {
+        println!("Player just shot!");
+    }
+    if action.pressed(Action::Down) {
+        d_pos.y -= 1.0;
+    }
+    if action.pressed(Action::Up) {
+        d_pos.y += 1.0;
+    }
+    if action.pressed(Action::Right) {
+        d_pos.x += 1.0;
+    }
+    if action.pressed(Action::Left) {
+        d_pos.x -= 1.0;
+    }
+
+    transform.translation.x += d_pos.x;
+    transform.translation.y += d_pos.y;
 }
